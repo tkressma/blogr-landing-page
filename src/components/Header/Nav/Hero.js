4abf1681f826e82331ffe1a531ad75eaeced44c1
Thomas Kressman <tkressma@oswego.edu>
2022-01-15 18:06:48 -0500

    Update hero typograpy

--- src/components/Header/Nav/Hero.js
+++ src/components/Header/Nav/Hero.js
@@ -2,9 +2,9 @@ import Accessibility from "../Accessibility"
 import styles from "./Hero.module.css";
 const Hero = () => {
     return (
-        <section class={styles.hero}>
-        <h1 class={styles["hero__title"]}>A modern publishing platform</h1>
-        <p>Grow your audience and build your online brand</p>
+        <section className={styles.hero}>
+        <h1 className={styles["hero__title"]}>A modern publishing platform</h1>
+        <p className={styles["hero__tagline"]}>Grow your audience and build your online brand</p>
 
         <Accessibility />
         </section>
