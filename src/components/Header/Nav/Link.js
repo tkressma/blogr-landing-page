1ad9fb5f8acdc9eb90b030f99f855d0937b079a6
Thomas Kressman <tkressma@oswego.edu>
2022-01-16 19:43:09 -0500

    Add arrow icons to nav links

--- src/components/Header/Nav/Link.js
+++ src/components/Header/Nav/Link.js
@@ -1,8 +1,11 @@
 import styles from "./Link.module.css";
+import lightArrow from "../../../assets/icon-arrow-light.svg"
 const Link = (props) => {
   return (
     <li className={styles.link}>
       <a href="#display">{props.name}</a>
+      <img src={lightArrow} />
+
     </li>
   );
 };
