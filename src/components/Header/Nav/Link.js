e26ecec1bb6af94bede46c89794869ee06d2f4bf
Thomas Kressman <tkressma@oswego.edu>
2022-01-18 13:21:17 -0500

    Add nav menu arrow animation

--- src/components/Header/Nav/Link.js
+++ src/components/Header/Nav/Link.js
@@ -1,11 +1,12 @@
 import styles from "./Link.module.css";
+import classNames from 'classnames';
 import lightArrow from "../../../assets/icon-arrow-light.svg"
-const Link = (props) => {
 
+const Link = (props) => {
   return (
-    <li key={props.key} className={`${styles.link} ${props.active ? styles.active : null}`} onClick={props.onClick} >
+    <li key={props.key} className={classNames(styles.link, props.active && styles.active)} onClick={props.onClick} >
       {props.name}
-      <img class={styles["nav__arrow"]} src={lightArrow} />
+      <img className={classNames(styles.animate, props.active && styles.rotate)} src={lightArrow} />
     </li>
   );
 };
