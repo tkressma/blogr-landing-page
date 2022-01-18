eb0ccba8ec2a588ae18ce0fc8a0ac65effba31e1
Thomas Kressman <tkressma@oswego.edu>
2022-01-18 16:03:37 -0500

    Add mobile nav submenu

--- src/components/Header/Nav/MobileNavItem.js
+++ src/components/Header/Nav/MobileNavItem.js
@@ -2,9 +2,19 @@ import styles from "./MobileNavItem.module.css"
 import classNames from "classnames"
 import darkArrow from "../../../assets/icon-arrow-dark.svg"
 const MobileNavItem = (props) => {
-    return <li className={classNames(styles["mobile-item"], props.active && styles.active)} onClick={props.onClick}>{props.name}
+    return (
+    <>
+    <li className={classNames(styles["mobile-item"], props.active && styles.active)} onClick={props.onClick}>
+    
+    {props.name}
+
           <img src={darkArrow} className={classNames(styles.animate, props.active && styles.rotate)} alt="Arrow for dropdown menu."/>
     </li>
+
+    {props.active && <section className={styles.submenu}>hello</section>}
+    </>
+    )
+
 }
 
 export default MobileNavItem
