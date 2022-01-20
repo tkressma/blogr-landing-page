7c62b60eac927ef82ec60a0c808913d5b4760fc7
Tom Kressman <tkressma@oswego.edu>
2022-01-19 21:24:32 -0500

    Various responsiveness changes in the header

--- src/components/Header/Nav/MobileNav/MobileNavSubMenu.js
+++ src/components/Header/Nav/MobileNav/MobileNavSubMenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./MobileNavSubMenu.module.css";
 import classNames from "classnames";
 const MobileNavSubMenu = (props) => {
--- src/components/Header/Nav/NavItem.js
+++ src/components/Header/Nav/NavItem.js
@@ -1,12 +1,20 @@
 import styles from "./NavItem.module.css";
-import classNames from 'classnames';
-import lightArrow from "../../../assets/icon-arrow-light.svg"
+import classNames from "classnames";
+import lightArrow from "../../../assets/icon-arrow-light.svg";
 
 const NavItem = (props) => {
   return (
-    <li key={props.key} className={classNames(styles.link, props.active && styles.active)} onClick={props.onClick} >
+    <li
+      key={props.key}
+      className={classNames(styles.item, props.active && styles.active)}
+      onClick={props.onClick}
+    >
       {props.name}
-      <img className={classNames(styles.animate, props.active && styles.rotate)} src={lightArrow} alt="Arrow for dropdown menu."/>
+      <img
+        className={classNames(styles.animate, props.active && styles.rotate)}
+        src={lightArrow}
+        alt="Arrow for dropdown menu."
+      />
     </li>
   );
 };
