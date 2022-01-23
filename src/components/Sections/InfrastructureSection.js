9c6aab16834c341d8adf8979ae05b3accade5425
Tom Kressman <tkressma@oswego.edu>
2022-01-22 21:48:53 -0500

    Add base html/css for insfrastructure section

---
+++ src/components/Sections/InfrastructureSection.js
@@ -0,0 +1,22 @@
+import styles from "./InfrastructureSection.module.css";
+import phones from "../../assets/illustration-phones.svg";
+import circles from "../../assets/bg-pattern-circles.svg";
+const InfrastructureSection = () => {
+  return (
+    <section className={styles.section}>
+      <article className={styles["section__article"]}>
+        <h2 className={styles["section__h2"]}>
+          State of the Art Infrastructure
+        </h2>
+        <p className={styles["section__p"]}>
+          With reliability and speed in mind, worldwide data centers provide the
+          backbone for ultra-fast connectivity. This ensures your site will load
+          instantly, no matter where your readers are, keeping your site
+          competitive.
+        </p>
+      </article>
+    </section>
+  );
+};
+
+export default InfrastructureSection;
--- src/components/Sections/Main.js
+++ src/components/Sections/Main.js
@@ -1,9 +1,12 @@
 import React from "react";
 import DesignSection from "./DesignSection";
+import InfrastructureSection from "./InfrastructureSection";
+
 const Main = () => {
   return (
     <main>
       <DesignSection />
+      <InfrastructureSection />
     </main>
   );
 };
