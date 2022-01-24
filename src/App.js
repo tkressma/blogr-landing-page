833e4a91924295cb577c8f73614246a02371b029
Tom Kressman <tkressma@oswego.edu>
2022-01-23 22:11:08 -0500

    Remove structure comments

--- src/App.js
+++ src/App.js
@@ -11,9 +11,3 @@ function App() {
 }
 
 export default App;
-
-// HEADER: Includes NAV (Logo and menu), as well as a section HERO (H1 title, p tagline, btn-group (start for free, learn more)
-// FIRST ARTICLE: Includes H2 section title, section img, two sections each with H3 titles
-// SECOND SECTION: Includes section img, H2 section title, section info
-// THIRD SECTION: Includes section img, two articles each with H3 titles and section info
-// FOOTER: Includes Logo and footer elements
