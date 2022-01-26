209392e61a97d2afa521874d6877b54a9bd27939
Tom Kressman <tkressma@oswego.edu>
2022-01-26 13:51:11 -0500

    Fix layout for sections

--- src/components/Sections/Main.js
+++ src/components/Sections/Main.js
@@ -1,14 +1,49 @@
 import React from "react";
-import DesignSection from "./DesignSection";
+import DesignSection from "./Section";
 import InfrastructureSection from "./InfrastructureSection";
-import ToolingSection from "./ToolingSection";
+import editor__desktop from "../../assets/illustration-editor-desktop.svg";
+import editor__mobile from "../../assets/illustration-editor-mobile.svg";
+import laptop__desktop from "../../assets/illustration-laptop-desktop.svg";
+import laptop__mobile from "../../assets/illustration-laptop-mobile.svg";
 
 const Main = () => {
+  const firstSectionContent = [
+    {
+      title: "Introducing an extensible editor",
+      info: "Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.",
+    },
+    {
+      title: "Robust content management",
+      info: "Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.",
+    },
+  ];
+
+  const lastSectionContent = [
+    {
+      title: "Free, Open, Simple",
+      info: "Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.",
+    },
+    {
+      title: "Powerful Tooling",
+      info: "Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.",
+    },
+  ];
+
   return (
     <main>
-      <DesignSection />
+      <DesignSection
+        sectionContent={firstSectionContent}
+        desktopImg={editor__desktop}
+        mobileImg={editor__mobile}
+        lastSection={false}
+      />
       <InfrastructureSection />
-      <ToolingSection />
+      <DesignSection
+        sectionContent={lastSectionContent}
+        desktopImg={laptop__desktop}
+        mobileImg={laptop__mobile}
+        lastSection={true}
+      />
     </main>
   );
 };
---
+++ src/components/Sections/Section.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import classNames from "classnames";
+import ArticleInfo from "./ArticleInfo";
+import styles from "./Section.module.css";
+
+const Section = (props) => {
+  const isMobile = useMediaQuery({ maxWidth: 1000 });
+
+  return (
+    <section className={styles.section}>
+      {!props.lastSection && (
+        <h2 className={styles["section__h2"]}>Designed for the future</h2>
+      )}
+
+      <div
+        className={classNames(
+          styles["section__img_container"],
+          props.lastSection && styles["section__img_container--second"]
+        )}
+      >
+        <img
+          className={styles["section__img"]}
+          src={isMobile ? props.mobileImg : props.desktopImg}
+        />
+      </div>
+
+      <article
+        className={classNames(
+          styles["section__article"],
+          props.lastSection && styles["section__article--second"]
+        )}
+      >
+        {props.sectionContent.map((content) => (
+          <ArticleInfo title={content.title} info={content.info} />
+        ))}
+      </article>
+    </section>
+  );
+};
+
+export default Section;
